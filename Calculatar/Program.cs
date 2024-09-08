e1230b1ae467e71839fc1823c3779011f8504081
Alexprodark <Tinabluerose2@gmail.com>
2024-09-09 01:05:32 +0200

    cool

---
+++ Calculatar/Program.cs
@@ -0,0 +1,40 @@
+﻿            Console.WriteLine("write a number");
+          
+            string n1 = Console.ReadLine();
+           
+            Console.WriteLine("what do u want? (+, -, x, /)");
+          
+            string op = Console.ReadLine();
+            
+            Console.WriteLine("write a second number");
+          
+            string n2 = Console.ReadLine();
+           
+            int num1 = Convert.ToInt32(n1);
+            int num2 = Convert.ToInt32(n2);
+
+            switch (op)
+            {
+                case "+":
+                    Console.WriteLine( num1 + num2 );
+                    break;
+                case "-":
+                    Console.WriteLine( num1 - num2 );
+                    break;
+                case "x":
+                    Console.WriteLine( num1 * num2 );
+                    break;
+                    
+                case "/":
+                    if (0 != num2)
+                    {
+                        Console.WriteLine(num1 / num2);
+                    }
+                    else
+                    {
+                        Console.WriteLine("you cant devide by 0");                       
+                    }
+                    break;
+
+            }
+            Console.ReadKey();
